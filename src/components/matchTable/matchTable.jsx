91705ce4d1fe5e183d0be0b87e18bbea80c2c387
lazaroq11 <lazaroq11@gmail.com>
2022-06-21 21:06:51 -0300

    feat:add matchTable component

--- src/components/matchTable/matchTable.jsx
+++ src/components/matchTable/matchTable.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import {Main, MatchContent} from "./styles"
 import Header from "../header/header";
 import { NativeSelect } from '@mui/material';
+import team from '../../assets/126.png'
 
 function MatchTable(){
 
@@ -17,13 +18,67 @@ function MatchTable(){
 
           <div class="content">
             <div class="content__info">
-                <p class="content__info__title"><strong>Classificatória</strong></p>
+                <p class="content__info__title"><strong>Fase Classificatória</strong></p>
                 <NativeSelect>
                 <option value={1}>Rodada 1</option>
                 <option value={2}>Rodada 2</option>
                 <option value={3}>Rodada 3</option>
                 </NativeSelect>
-            </div>
+                </div>
+                
+                <div class="content__card">
+                  <div class="content__card__teams">
+                  <p>Jogo <strong>1</strong> | <strong>1 Rodada</strong> | <strong>POLIESPORTIVO DO RIACHO</strong></p>
+                  <span>23/10 - 19h00</span>
+                  <div class="content__card__teams__matches">
+                  <p>SADA CRUZEIRO</p>  
+                  <img src={team} alt="Cruzeiro" width="120" height="120"/>
+                  <div class="content__card__teams__matches__setsPoints">
+                  <span>25</span>
+                  <span>25</span>
+                  <span>25</span>
+                  </div> 
+                  <p>3 - 0</p>
+                  <div class="content__card__teams__matches__setsPoints">
+                  <span>25</span>
+                  <span>25</span>
+                  <span>25</span>
+                  </div> 
+                  <img class="content__card__teams__matches__imgTeam" src={team} alt="Cruzeiro" width="120" height="120" />
+                  <p>SADA CRUZEIRO</p>
+                  
+                  </div>
+                  </div>
+                </div>
+
+
+                <div class="content__card">
+                  <div class="content__card__teams">
+                  <p>Jogo <strong>1</strong> | <strong>1 Rodada</strong> | <strong>POLIESPORTIVO DO RIACHO</strong></p>
+                  <span>23/10 - 19h00</span>
+                  <div class="content__card__teams__matches">
+                  <p>SADA CRUZEIRO</p>  
+                  <img src={team} alt="Cruzeiro" width="120" height="120"/>
+                  <div class="content__card__teams__matches__setsPoints">
+                  <span>25</span>
+                  <span>25</span>
+                  <span>25</span>
+                  </div> 
+                  <p>3 - 0</p>
+                  <div class="content__card__teams__matches__setsPoints">
+                  <span>25</span>
+                  <span>25</span>
+                  <span>25</span>
+                  </div> 
+                  <img class="content__card__teams__matches__imgTeam" src={team} alt="Cruzeiro" width="120" height="120" />
+                  <p>SADA CRUZEIRO</p>
+                  
+                  </div>
+                  </div>
+                </div>
+
+                
+            
           </div>
         </MatchContent>
        </Main>
--- src/components/matchTable/styles.jsx
+++ src/components/matchTable/styles.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Main = styled.div`
-    
+    width:100%;
+    height:100vh;
 `;
 
 export const MatchContent = styled.div`
@@ -18,5 +19,48 @@ margin-left:10px;
             font-style:italic;
           }
         }
+
+        &__card{
+          display:flex;
+          flex-direction:column;
+          align-items:center;
+          justify-content:center;
+          font-size:15px;
+
+         
+
+          &__teams{
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            padding-top:10px;
+            
+          
+
+            &__matches{
+              display:flex;
+              flex-direction:row;
+              align-items:center;
+              padding-bottom:60px;
+
+              &__setsPoints{
+                display:flex;
+                flex-direction:column;
+                justify-content:space-between;
+                
+               
+                span{
+                  padding-top:5px;
+                  padding-left:20px;
+                  padding-right:20px;
+                }
+                
+              }
+             
+              
+            }
+          }
+        }
+        
     }
 `;
