dfc1f2b498c902779761c57e5bd6059fd1b00edf
lazaroq11 <lazaroq11@hotmail.com>
2022-06-27 14:46:39 -0300

    feat:get in matchTable

--- src/components/matchTable/matchTable.jsx
+++ src/components/matchTable/matchTable.jsx
@@ -33,16 +33,14 @@ function MatchTable(){
             <div class="content__info">
                 <p class="content__info__title"><strong>Fase Classificatória</strong></p>
                 <NativeSelect>
-                <option value={1}>Rodada 1</option>
-                <option value={2}>Rodada 2</option>
-                <option value={3}>Rodada 3</option>
+                <option value={confrontation.round}>{confrontation.round}</option>
                 </NativeSelect>
                 </div>
                 
                 <div class="content__card">
                   <div class="content__card__teams">
-                  <p>Jogo <strong>1</strong> | <strong>1 Rodada</strong> | <strong>POLIESPORTIVO DO RIACHO</strong></p>
-                  <span>23/10 - 19h00</span>
+                  <p>Jogo <strong>{confrontation.id}</strong> | <strong>{confrontation.round} Rodada</strong> | <strong>POLIESPORTIVO DO RIACHO</strong></p>
+                  <span>{confrontation.scheduling}</span>
                   <div class="content__card__teams__icons">
                   <Link to="/" class="content__card__teams__icons__edit">{<BiEditAlt/>}</Link>
                   <Link to="/" class="content__card__teams__icons__delete">{<MdDelete/>}</Link> 
@@ -51,52 +49,25 @@ function MatchTable(){
                   <p>SADA CRUZEIRO</p>  
                   <img src={team} alt="Cruzeiro" width="120" height="120"/>
                   <div class="content__card__teams__matches__setsPoints">
-                  <span>25</span>
-                  <span>25</span>
-                  <span>25</span>
+                  <span>{confrontation.set1_points_host}</span>
+                  <span>{confrontation.set2_points_host}</span>
+                  <span>{confrontation.set3_points_host}</span>
+                  <span>{confrontation.set4_points_host = "NULL" ? "": confrontation.set4_points_host}</span>
+                  <span>{confrontation.set5_points_host = "NULL" ? "": confrontation.set5_points_host}</span>
                   </div> 
-                  <p>3 - 0</p>
+                  <p>{confrontation.result_host} - {confrontation.result_guest}</p>
                   <div class="content__card__teams__matches__setsPoints">
-                  <span>25</span>
-                  <span>25</span>
-                  <span>25</span>
+                  <span>{confrontation.set1_points_guest}</span>
+                  <span>{confrontation.set2_points_guest}</span>
+                  <span>{confrontation.set3_points_guest}</span>
+                  <span>{confrontation.set4_points_host = "NULL" ? "": confrontation.set4_points_host}</span>
+                  <span>{confrontation.set5_points_host = "NULL" ? "": confrontation.set5_points_host}</span>
                   </div> 
                   <img class="content__card__teams__matches__imgTeam" src={team} alt="Cruzeiro" width="120" height="120" />
                   <p>SADA CRUZEIRO</p>
                   </div>
                   </div>
-                </div>
-                <div class="content__card">
-                  <div class="content__card__teams">
-                  <p>Jogo <strong>1</strong> | <strong>1 Rodada</strong> | <strong>POLIESPORTIVO DO RIACHO</strong></p>
-                  <span>23/10 - 19h00</span>
-                  <div class="content__card__teams__icons">
-                  <Link to="/" class="content__card__teams__icons__edit">{<BiEditAlt/>}</Link>
-                  <Link to="/" class="content__card__teams__icons__delete">{<MdDelete/>}</Link> 
-                  </div>
-                  <div class="content__card__teams__matches">
-                  <p>SADA CRUZEIRO</p>  
-                  <img src={team} alt="Cruzeiro" width="120" height="120"/>
-                  <div class="content__card__teams__matches__setsPoints">
-                  <span>25</span>
-                  <span>25</span>
-                  <span>25</span>
-                  </div> 
-                  <p>3 - 0</p>
-                  <div class="content__card__teams__matches__setsPoints">
-                  <span>25</span>
-                  <span>25</span>
-                  <span>25</span>
-                  </div> 
-                  <img class="content__card__teams__matches__imgTeam" src={team} alt="Cruzeiro" width="120" height="120" />
-                  <p>SADA CRUZEIRO</p>
-                  
-                  </div>
-                  </div>
-                </div>
-
-                
-            
+                </div>      
           </div>
         ))}
         </MatchContent>
